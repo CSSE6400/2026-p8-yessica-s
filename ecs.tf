db58151b5c5bb46b2a92d129c1fa3121ba194ead
Yessica <yessicamsamtani@gmail.com>
2026-04-22 11:40:08 +1000

    Prac8

--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,102 @@
+# resource "aws_ecs_cluster" "taskoverflow" {
+#   name = "taskoverflow"
+# }
+#
+# resource "aws_ecs_task_definition" "taskoverflow" {
+#   family                   = "taskoverflow"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   cpu                      = 1024
+#   memory                   = 2048
+#   execution_role_arn       = data.aws_iam_role.lab.arn
+#   task_role_arn            = data.aws_iam_role.lab.arn
+#
+#   container_definitions = <<DEFINITION
+# [
+#   {
+#     "image": "${docker_registry_image.taskoverflow.name}",
+#     "cpu": 1024,
+#     "memory": 2048,
+#     "name": "taskoverflow",
+#     "networkMode": "awsvpc",
+#     "portMappings": [
+#       {
+#         "containerPort": 6400,
+#         "hostPort": 6400
+#       }
+#     ],
+#     "environment": [
+#       {
+#         "name": "SQLALCHEMY_DATABASE_URI",
+#         "value": "postgresql://${local.database_username}:${local.database_password}@${aws_db_instance.taskoverflow_database.address}:${aws_db_instance.taskoverflow_database.port}/${aws_db_instance.taskoverflow_database.db_name}"
+# # The following to use psycopg v3.
+# #        "value": "postgresql+psycopg://${local.database_username}:${local.database_password}@${aws_db_instance.taskoverflow_database.address}:${aws_db_instance.taskoverflow_database.port}/${aws_db_instance.taskoverflow_database.db_name}"
+#       }
+#     ],
+#     "logConfiguration": {
+#       "logDriver": "awslogs",
+#       "options": {
+#         "awslogs-group": "/taskoverflow/db",
+#         "awslogs-region": "us-east-1",
+#         "awslogs-stream-prefix": "ecs",
+#         "awslogs-create-group": "true"
+#       }
+#     }
+#   }
+# ]
+# DEFINITION
+# }
+#
+# resource "aws_ecs_service" "taskoverflow" {
+#   name            = "taskoverflow"
+#   cluster         = aws_ecs_cluster.taskoverflow.id
+#   task_definition = aws_ecs_task_definition.taskoverflow.arn
+#   desired_count   = 1
+#   launch_type     = "FARGATE"
+#
+#   network_configuration {
+#     subnets             = data.aws_subnets.private.ids
+#     security_groups     = [aws_security_group.taskoverflow.id]
+#     assign_public_ip    = true
+#   }
+#
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.taskoverflow.arn
+#     container_name   = "taskoverflow"
+#     container_port   = 6400
+#   }
+# }
+#
+# resource "aws_security_group" "taskoverflow" {
+#   name = "taskoverflow"
+#   description = "TaskOverflow Security Group"
+#
+#   ingress {
+#     from_port = 6400
+#     to_port = 6400
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   ingress {
+#     from_port = 22
+#     to_port = 22
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   egress {
+#     from_port = 0
+#     to_port = 0
+#     protocol = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   tags = {
+#     name = "taskoverflow_security_group"
+#   }
+# }
+
 resource "aws_ecs_cluster" "taskoverflow" {
   name = "taskoverflow"
 }
@@ -29,8 +128,6 @@ resource "aws_ecs_task_definition" "taskoverflow" {
       {
         "name": "SQLALCHEMY_DATABASE_URI",
         "value": "postgresql://${local.database_username}:${local.database_password}@${aws_db_instance.taskoverflow_database.address}:${aws_db_instance.taskoverflow_database.port}/${aws_db_instance.taskoverflow_database.db_name}"
-# The following to use psycopg v3.
-#        "value": "postgresql+psycopg://${local.database_username}:${local.database_password}@${aws_db_instance.taskoverflow_database.address}:${aws_db_instance.taskoverflow_database.port}/${aws_db_instance.taskoverflow_database.db_name}"
       }
     ],
     "logConfiguration": {
@@ -55,9 +152,9 @@ resource "aws_ecs_service" "taskoverflow" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets             = data.aws_subnets.private.ids
-    security_groups     = [aws_security_group.taskoverflow.id]
-    assign_public_ip    = true
+    subnets          = data.aws_subnets.private.ids
+    security_groups  = [aws_security_group.taskoverflow.id]
+    assign_public_ip = true
   }
 
   load_balancer {
@@ -68,27 +165,27 @@ resource "aws_ecs_service" "taskoverflow" {
 }
 
 resource "aws_security_group" "taskoverflow" {
-  name = "taskoverflow"
+  name        = "taskoverflow"
   description = "TaskOverflow Security Group"
 
   ingress {
-    from_port = 6400
-    to_port = 6400
-    protocol = "tcp"
+    from_port   = 6400
+    to_port     = 6400
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
